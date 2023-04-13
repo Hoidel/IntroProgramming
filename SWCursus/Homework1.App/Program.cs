6a9e452d5bf95e8c92d28969c0f039f5cbb832cf
Lennart Theil <L.Theil@chipsoft.com>
2023-04-13 15:37:27 +0200

    Beginnetje gemaakt met huiswerk1

---
+++ SWCursus/Homework1.App/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goedendag. In welk jaar bent u geboren? Geef aub het antwoord in cijfers.");
+
+            Console.WriteLine("In welke maand bent u geboren? Geef aub het antwoord in cijfers.");
+
+            Console.WriteLine("Op welke dag bent u geboren? Geef aub het antwoord in cijfers.");
+        }
+    }
+}
---
+++ SWCursus/Homework1.Tests/BirthdayCalculatorTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+
+namespace Homework1.Tests
+{
+    public class BirthdayCalculatorTests
+    {
+        private BirthdayCalculator calculator = new BirthdayCalculator();
+        int yearToday = 2023;
+        int monthToday = 4;
+        int dayToday = 13;
+
+        [Fact]
+        public void GetAgeInYears_YearLowerMonthLowerDayLower_CorrectOutput()
+        {
+            int yearOfBirth = 1960;
+            int monthOfBirth = 3;
+            int dayOfBirth = 3;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(63, output);
+        }
+
+        [Fact]
+        public void GetAgeInYears_YearLowerMonthLowerDayEqual_CorrectOutput()
+        {
+            int yearOfBirth = 1972;
+            int monthOfBirth = 3;
+            int dayOfBirth = 13;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(51, output);
+        }
+
+        [Fact]
+        public void GetAgeInYears_YearLowerMonthLowerDayHigher_CorrectOutput()
+        {
+            int yearOfBirth = 1972;
+            int monthOfBirth = 3;
+            int dayOfBirth = 19;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(51, output);
+        }
+
+        [Fact]
+        public void GetAgeInYears_YearInPastMonthInPastDayInPast_CorrectOutput()
+        {
+            int yearOfBirth = 1960;
+            int monthOfBirth = 3;
+            int dayOfBirth = 3;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(63, output);
+        }
+
+        [Fact]
+        public void GetAgeInYears_YearInPastMonthInPastDayToday_CorrectOutput()
+        {
+            int yearOfBirth = 1972;
+            int monthOfBirth = 3;
+            int dayOfBirth = 13;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(51, output);
+        }
+
+        [Fact]
+        public void GetAgeInYears_YearInPastMonthInPastDayinFuture_CorrectOutput()
+        {
+            int yearOfBirth = 1972;
+            int monthOfBirth = 3;
+            int dayOfBirth = 19;
+
+            int output = calculator.GetAgeInYears(yearToday, monthToday, dayToday, yearOfBirth, monthOfBirth, dayOfBirth);
+
+            Assert.Equal(51, output);
+        }
+    }
+}
