02d5a36b9756d583c7d16cb736989bfa79da08ca
Jarn0Jarn0 <jarno_sluik@hotmail.com>
2023-05-20 12:10:15 +0200

    Nieuwe branch Jarno

--- SWCursus/Homework1/DayCalculator.cs
+++ SWCursus/Homework1/DayCalculator.cs
@@ -14,13 +14,28 @@
         /// <returns></returns>
         public bool IsLeapYear(int year)
         {
+            if (year % 400 == 0)
+            { 
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else if (year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             //Adjust the method to the following criteria:
             //    Every year that is exactly divisible by four is a leap year, 
             //    except for years that are exactly divisible by 100, 
             //    but these centurial years are leap years if they are exactly divisible by 400.
             //    For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are.[7]
             //    https://en.wikipedia.org/wiki/Leap_year
-            return false;
         }
 
         /// <summary>
@@ -30,7 +45,14 @@
         /// <returns></returns>
         public int GetNumberOfDaysInYear(int year)
         {
-            return 9000;
+            if (!IsLeapYear(year) == true)
+            {
+                return 365;
+            }
+            else
+            {
+                return 366;
+            }
         }
 
         /// <summary>
@@ -40,7 +62,59 @@
         /// <returns></returns>
         public int GetNumberOfDaysInMonth(int month, int year)
         {
-            return 9000;
+            if (GetNumberOfDaysInYear(year) == 366 && month == 2)
+            {
+                return 29;
+            }
+            else if (GetNumberOfDaysInYear(year) == 365 && month == 2)
+            {
+                return 28;
+            }
+            else if (month == 1)
+            {
+                return 31;
+            }
+            else if (month == 3)
+            {
+                return 31;
+            }
+            else if (month == 4)
+            {
+                return 30;
+            }
+            else if (month == 5)
+            {
+                return 31;
+            }
+            else if (month == 6)
+            {
+                return 30;
+            }
+            else if (month == 7)
+            {
+                return 31;
+            }
+            else if (month == 8)
+            {
+                return 31;
+            }
+            else if (month == 9)
+            {
+                return 30;
+            }
+            else if (month == 10)
+            {
+                return 31;
+            }
+            else if (month == 11)
+            {
+                return 30;
+            }
+            else
+            {
+                return 31;
+            }
+
         }
 
         /// <summary>
@@ -51,7 +125,7 @@
         /// <returns></returns>
         public int GetAdjustedDelta(int delta)
         {
-            return 9000;
+            return delta % 7;
         }
 
         /// <summary>
@@ -60,8 +134,8 @@
         /// <param name="delta">The number of days the dayNumber needs to be adjusted. Positive for a change in the future, negative for a backwards change</param>
         /// <returns></returns>
         public int GetNewDayOfTheWeek(int dayNumber, int delta)
-        {
-            return 9000;
+        {     
+            return (GetAdjustedDelta(delta) + dayNumber % 7) % 7;
         }
 
         /// <summary>
@@ -75,7 +149,25 @@
 
         public string GetDay(int dayNumber)
         {
-            return "";
+            switch (dayNumber)
+            {
+                case 0:
+                    return"zondag";
+                case 1:
+                    return"maandag";
+                case 2:
+                    return"dinsdag";
+                case 3:
+                    return"woensdag";
+                case 4:
+                    return"donderdag";
+                case 5:
+                    return"vrijdag";
+                case 6:
+                    return"zaterdag";
+                default:
+                    return"onbekende dag";
+            }
         }
     }
 }
